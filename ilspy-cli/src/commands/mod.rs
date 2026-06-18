9fac334dd0eab49f427ae76877f99b909d6db606
Alexander Kiselev <alex@akiselev.com>
2026-06-18 15:46:12 -0700

    removed ilspy-cli into its own repo

--- ilspy-cli/src/commands/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod list;
-pub mod decompile;
-pub mod search;
-pub mod info;
-pub mod detect;
-pub mod doctor;
--- ilspy-cli/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod bridge;
-pub mod cli;
-pub mod commands;
-pub mod error;
-pub mod format;
-pub mod ilspy;
